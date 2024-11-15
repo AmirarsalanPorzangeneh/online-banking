52c2c74e51f4b7be8783483e4bb1415eecccba44
Amirarsalan <amirzangene16@gmail.com>
2024-11-16 02:14:51 +0330

    Monytransfer

--- src/Pages/moneyTransfer/MoneyTransfer.jsx
+++ src/Pages/moneyTransfer/MoneyTransfer.jsx
@@ -1,23 +1,85 @@
-import React,{useState} from 'react'
-import PooyaPass from '../../Components/pooyaPass/PooyaPass'
-
+import React, { useState } from "react";
+import PooyaPass from "../../Components/pooyaPass/PooyaPass";
+import Input from "../../Components/input/Input";
+import CustomDate from "../../Components/CustomDate/CustomDate";
+import Buttons from "../../Components/buttons/Buttons";
+import { useForm } from "react-hook-form";
+import Form from "../../Components/form/Form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import "./monyTransfer.css";
 
 export default function MoneyTransfer() {
-  const [passAppear, setPassAppear] = useState(false)
+  const [passAppear, setPassAppear] = useState(false);
   const passHandler = () => {
-    setPassAppear(!passAppear )
-  }
+    setPassAppear(!passAppear);
+  };
   const oncloseHandler = () => {
-    setPassAppear(false)
-  }
-  console.log(passAppear)
+    setPassAppear(false);
+  };
+  console.log(passAppear);
+
+  const schema = yup.object().shape({
+    amount: yup
+      .string()
+      .required("لطفا مبلغ را وارد کنید")
+      .test(
+        "is-numeric-and-greater-than-10000",
+        "حداقل مبلغ موردنیاز جهت افتتاح حساب 10000 تومان است",
+        (value) => {
+          const numberValue = Number(value);
+          return !isNaN(numberValue) && numberValue >= 10000;
+        }
+      ),
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault;
+  };
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
   return (
     <>
-     
-      <button className='text-white' onClick={passHandler}>click</button>
-
-      {passAppear && <PooyaPass onClose={oncloseHandler}/>}
-      
+      <Form
+        FormTitle="انتقال وجه"
+        onSubmit={handleSubmit(onSubmit)}
+        className="	absolute top-36">
+        <Input
+          dir="ltr"
+          type="number"
+          inputName="کارت مبدا"
+          placeholder="0000 - 0000 - 0000 - 0000"
+        />
+        <Input
+          dir="ltr"
+          type="number"
+          inputName="کارت مقصد"
+          placeholder="0000 - 0000 - 0000 - 0000"
+        />
+        <Input
+          dir="ltr"
+          type="text"
+          id="amount"
+          inputName="مبلغ(تومان)"
+          register={register("amount")}
+        />
+        {/* <p style={{ color: "red", paddingBottom: "10px" }}>
+          {errors.amount?.message}
+        </p> */}
+        <Input inputName="CVV2" dir="ltr" />
+        <CustomDate label="تاریخ انقضا" name="date" control={control} />
+        <Buttons btnName="تایید" onClick={passHandler} />
+        {passAppear && <PooyaPass onClose={oncloseHandler} />}
+      </Form>
     </>
-  )
+  );
 }
